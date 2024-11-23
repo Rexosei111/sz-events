d6c9d82fc9dd97e175d9ab5e1e22b144ebfe368c
Kyei Samuel Osei <kyeisamuel931@gmail.com>
2024-11-23 18:24:32 +0000

    spirit zone

--- src/components/users/navBar.js
+++ src/components/users/navBar.js
@@ -53,7 +53,7 @@ export const CustomNavbar = ({ onSelect, activeKey, ...props }) => {
         onClick={handleNavigation}
         style={{ color: theme.palette.text.primary, fontWeight: 700 }}
       >
-        Spiritzone
+        Spirit zone
       </Navbar.Brand>
       <Nav onSelect={onSelect} activeKey={activeKey}>
         {!matches && (
