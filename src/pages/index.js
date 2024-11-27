01d29176f959633c8ad65ba2614a174372224ca6
Kyei Samuel Osei <kyeisamuel931@gmail.com>
2024-11-27 02:25:10 +0000

    Redirect to registeration page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,23 +12,29 @@ export default function Home() {
   const router = useRouter();
   useEffect(() => {
     if (router.isReady === true) {
-      router.push("/events");
+      router.push("/events/638dd663-90fe-4fa5-8021-50f7d66cd9b1");
     }
   }, [router.isReady]);
   return (
     <>
       <Head>
-        <title>SZ events</title>
-        <meta property="og:title" content="SZ events" />
+        <title>GOOD SHEPHERD CONFERENCE: THE CALL</title>
+        <meta
+          property="og:title"
+          content="GOOD SHEPHERD CONFERENCE: THE CALL"
+        />
         <meta
           property="og:description"
-          content="Get all events organised by Spirit zone in one place."
+          content="Good Shepherd Conference (GSC) is a program the Lord is using as an avenue to equip his people to become good shepherds after the pattern of Jesus Christ. It’s an encounter that leaves participants with a hunger to make an impact. GSC is a channel the Lord is using to raise for himself, men after his own heart"
         />
         <meta
           property="og:image"
-          content="https://sz-event-bucket.s3.eu-west-3.amazonaws.com/af37b38a-061e-4393-a026-4f1e500556b4.png"
+          content="https://szevent-bucket.s3.eu-north-1.amazonaws.com/d906118e-dd1c-4a84-92ef-cc9f847c1d09.jpeg"
         />
-        <meta property="og:url" content="https://sz-events.vercel.app/events" />
+        <meta
+          property="og:url"
+          content="https://gsc.szfamily.org/events638dd663-90fe-4fa5-8021-50f7d66cd9b1"
+        />
         <meta property="og:type" content="website" />
       </Head>
       <Stack height={300} alignItems={"center"} justifyContent={"center"}>
